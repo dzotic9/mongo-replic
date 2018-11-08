e48d6eb3cee47ba6b8370691decc562480efdeba
dzotic9 <dzotic9@gmail.com>
2018-11-08 17:23:48 +0200

    Update scaleIn.js

--- scripts/scaleIn.js
+++ scripts/scaleIn.js
@@ -93,6 +93,7 @@ jelastic.marketplace.console.WriteLog("oConfig ->" + oConfig);
     oConfig = oConfig.match(/{[\s\tA-Za-z\n\w:,.()\[\]{}\\"]+}/g);
 	jelastic.marketplace.console.WriteLog("oConfig3 ->" + oConfig);
     oConfig = (oConfig.length > 0) ? oConfig[0] : oConfig;
+	jelastic.marketplace.console.WriteLog("oConfig4 ->" + oConfig);
     oConfig = toNative(new JSONObject((oConfig)));
     oConfigMembers = oConfig.members;
 	
